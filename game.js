4f5987eb0928ef6a634f91881cfe59edf1abe532
jferrando <jorgeparent@gmail.com>
2013-08-16 17:47:15 +0200

    initial project

---
+++ game.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById('game');
+var ctx = canvas.getContext && canvas.getContext('2d');
+
+if (!ctx) {
+	//No 2d context available
+	alert('Please, update your browser');
+} else {
+	startGame();
+}
+
+function startGame() {
+	//yellow rectangle 
+	ctx.fillStyle = "#FFFF00";
+	ctx.fillRect(50,100,380,400);
+
+	//blue semitransparent rectangle
+	ctx.fillStyle = "rgba(0,0,128,0.5)";
+	ctx.fillRect(0,50,380,400);
+}
